993524f2589ce9d601d012b12bc1eaee57d9ede0
trọng thành <thanhtrong.nguyen1602@gmail.com>
2021-12-29 08:51:57 +0700

    create private root

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './App.css';
+import { getToken } from './Commons/storage';
 import NavigationComponent from './NavigationComponent';
 import CovidTrackComponent from './Pages/CovidTrackComponent';
 import CreateVehicleComponent from './Pages/CreateVehicleComponent';
@@ -15,36 +21,56 @@ import VehicleComponent from './Pages/VehicleComponent';
 
 function App() {
   const queryClient = new QueryClient();
+  const PrivateRouteWithAuth = ({ component: Component, ...rest }: any) => (
+    <Route
+      {...rest}
+      render={(props) =>
+        getToken() ? <Component {...props} /> : <Redirect to='/login' />
+      }
+    />
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         <div>
           <NavigationComponent />
           <Switch>
-            <Route exact path='/'>
-              <HomeComponent />
-            </Route>
+            <PrivateRouteWithAuth exact path='/' component={HomeComponent} />
+
             <Route exact path='/login'>
               <LoginComponent />
             </Route>
             <Route exact path='/logup'>
               <LogupComponent />
             </Route>
-            <Route exact path='/detailUser'>
-              <DetailUserComponent />
-            </Route>
-            <Route exact path='/vehicle'>
-              <VehicleComponent />
-            </Route>
-            <Route exact path='/create-vehicle'>
-              <CreateVehicleComponent />
-            </Route>
-            <Route exact path='/edit-vehicle/:id'>
-              <EditVehicleComponent />
-            </Route>
-            <Route exact path='/covid-track'>
-              <CovidTrackComponent />
-            </Route>
+            <PrivateRouteWithAuth
+              exact
+              path='/detailUser'
+              component={DetailUserComponent}
+            />
+            <PrivateRouteWithAuth
+              exact
+              path='/vehicle'
+              component={VehicleComponent}
+            />
+            <PrivateRouteWithAuth
+              exact
+              path='/create-vehicle'
+              component={CreateVehicleComponent}
+            />
+
+            <PrivateRouteWithAuth
+              exact
+              path='/edit-vehicle/:id'
+              component={EditVehicleComponent}
+            />
+
+            <PrivateRouteWithAuth
+              exact
+              path='/covid-track'
+              component={CovidTrackComponent}
+            />
           </Switch>
         </div>
       </Router>
